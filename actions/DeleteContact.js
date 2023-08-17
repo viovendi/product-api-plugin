3994cdf1f782f7d64df58e71189e82de6b770cb7
Volodymyr Machula <machulav@gmail.com>
2023-08-17 11:51:57 +0200

    Enrich error message

--- actions/DeleteContact.js
+++ actions/DeleteContact.js
@@ -68,7 +68,12 @@ async function handler({ inputParameters, configurationParameters, action }) {
 
     // TODO: cover error handling with unit tests
     if (error.response) {
-      errorMessage = `Error ${error.response.status}: ${error.response.data?.user_message || error.response.data?.developer_message || error.response.statusText}`;
+      if (error.response.data?.user_message.includes('Contact is not eligible for deletion'))
+        // Enrich error message with more details
+        errorMessage = `Error ${error.response.status}: Contact is not eligible for deletion. There is at least one active or unapproved booking for a running event for this contact. Therefore the personal data of this contact cannot be deleted. Please cancel or reject the respective booking(s) before deleting the contact.`;
+      else {
+        errorMessage = `Error ${error.response.status}: ${error.response.data?.user_message || error.response.data?.developer_message || error.response.statusText}`;
+      }
     } else if (error.request) {
       // The request was made but no response was received
       errorMessage = "No response received from server.";
